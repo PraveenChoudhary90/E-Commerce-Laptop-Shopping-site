d82070d8f3522d21614ee06d8495d2c44461cffb
praveen choudhary <pc852180@gmail.com>
2025-04-01 13:58:38 +0530

    E-Commerce-site

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -6,8 +6,21 @@ import { MdLocalGroceryStore } from "react-icons/md";
 import { FaCircleUser } from "react-icons/fa6";
 import img1 from "../Images/logo1.png";
 import "../css/header.css"
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 const Header = ()=>{
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+
+
     return(
         <>
         <div id="header">
@@ -19,10 +32,29 @@ const Header = ()=>{
              <FaSearch />
              <FaHeart />
              <FaUser />
-             <MdLocalGroceryStore />
-             <FaCircleUser />
+             <MdLocalGroceryStore  />
+             <FaCircleUser onClick={handleShow} />
             </div>
+             
         </div>
+        <Button variant="primary">
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
         </>
     )
 }
