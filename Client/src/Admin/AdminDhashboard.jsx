0de7eb202832e1bb1b22fa32991ff5b9c768fbf3
praveen choudhary <pc852180@gmail.com>
2025-04-02 00:36:07 +0530

    E-Commerce-Site

--- Client/src/Admin/AdminDhashboard.jsx
+++ Client/src/Admin/AdminDhashboard.jsx
@@ -1,9 +1,26 @@
-
-
+import Button from 'react-bootstrap/Button';
+import "../css/dashboard.css"
+import { useNavigate } from 'react-router-dom';
 const Dashboard = ()=>{
+  const navigate = useNavigate();
+    const logout = ()=>{
+        localStorage.clear();
+        navigate("/");
+        alert("Your are Logout SuccessFully");
+        
+    }
     return(
         <>
-        <h1>Admin Dashboard</h1>
+        <div id="admindashboard">
+        <h1>Welcome to Admin Dashboard</h1>
+        </div>
+        <div id="data1">
+          Welcome :{localStorage.getItem("adminname")} 
+        </div>
+        <div id="data2">
+             <Button variant="primary" onClick={logout}>Logout</Button>
+        </div>
+
         </>
     )
 }
